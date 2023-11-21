aa1993e193bf1ce5593e31684d289f6d90e5ebb9
srachuri123 <sujan.k.rachuri@Vanderbilt.edu>
2023-11-21 14:50:18 -0500

    Fixed server index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
-const process = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const client = require('twilio')(
   process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN,
+  process.env.TWILIO_AUTH_TOKEN
 );
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
@@ -20,7 +19,6 @@ app.get('/api/greeting', (req, res) => {
 
 app.post('/api/messages', (req, res) => {
   res.header('Content-Type', 'application/json');
-  console.log("Twilio Phone Number:", process.env.TWILIO_PHONE_NUMBER);
   client.messages
     .create({
       from: process.env.TWILIO_PHONE_NUMBER,
