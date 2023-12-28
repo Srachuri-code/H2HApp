6d3e67e5d32004cdea41985220931285d03d233c
eliasxlevy <elias.levy@vanderbilt.edu>
2023-12-28 11:00:12 -0500

    Style phone numbers and cleanup

--- src/container/Organizations/Organizations.jsx
+++ src/container/Organizations/Organizations.jsx
@@ -13,6 +13,7 @@ const Organizations = ({ onSelectOrganization }) => {
 
   return (
     <div className="app__organizations-container">
+      <h2>Choose an organization:</h2>
       <ul className="app__organizations">
         {predefinedOrganizations.map((organization) => (
           <li
@@ -29,71 +30,3 @@ const Organizations = ({ onSelectOrganization }) => {
 };
 
 export default Organizations;
-
-
-/*
-import React, { useEffect, useState } from 'react';
-import { PhoneNumbers, SMSForm } from '..';
-import { db } from '../../firebase';
-import './Organizations.css';
-
-const predefinedOrganizations = ['Organization A', 'Organization B', 'Organization C'];
-
-const Organizations = ({ onSelectOrganization }) => {
-  // const [organizations, setOrganizations] = useState([]);
-  const [selectedOrganization, setSelectedOrganization] = useState('');
-
-  const handleClick = (organization) => {
-    setSelectedOrganization(organization);
-    onSelectOrganization(organization);
-  }
-
-  // useEffect(() => {
-  //   const fetchOrganizations = async () => {
-  //     try {
-  //       const organizationsCollection = db.collection('input-group');
-  //       const snapshot = await organizationsCollection.get();
-  //       const organizationsData = snapshot.docs.map((doc) => doc.data().organization);
-  //       setOrganizations(organizationsData);
-  //     } catch (error) {
-  //       console.error('Error fetching organizations:', error);
-  //     }
-  //   };
-
-  //   fetchOrganizations();
-  // }, []);
-
-  // const handleClick = (organization) => {
-  //   setSelectedOrganization(organization);
-  // };
-
-  // const handlePhoneNumberClick = (phoneNumber) => {
-  //   setSelectedPhoneNumber(phoneNumber);
-  // };
-
-  return (
-    <div className="app__organizations-container">
-      <ul className="app__organizations">
-        {predefinedOrganizations.map((organization) => (
-          <li
-            className={`app__organization-item ${selectedOrganization === organization ? 'active' : ''}`}
-            key={organization}
-            onClick={() => setSelectedOrganization(organization)}
-          >
-            {organization}
-          </li>
-        ))}
-      </ul>
-      {selectedOrganization && (
-        <PhoneNumbers 
-          organization={selectedOrganization} 
-          onPhoneNumberClick={handlePhoneNumberClick} 
-        />
-      )}
-    </div>
-  );
-};
-
-export default Organizations;
-
-*/
--- src/container/PhoneNumbers/PhoneNumbers.jsx
+++ src/container/PhoneNumbers/PhoneNumbers.jsx
@@ -17,11 +17,11 @@ const PhoneNumbers = ({ organization, onPhoneNumbersChange }) => {
   }, [organization, onPhoneNumbersChange]);
 
   return (
-    <div className="phone-numbers-container">
-      {organization && <h2>Phone Numbers for {organization}</h2>}
-      <ul className="phone-numbers-list">
+    <div className="app__phone-numbers-container">
+      {organization && <h2>Phone Numbers for {organization}:</h2>}
+      <ul className="app__phone-numbers">
         {phoneNumbers.map((phoneNumber, index) => (
-          <li key={index} className="app__organization-item">
+          <li key={index} className="app__phone-numbers-item">
             {phoneNumber}
           </li>
         ))}
@@ -31,58 +31,3 @@ const PhoneNumbers = ({ organization, onPhoneNumbersChange }) => {
 };
 
 export default PhoneNumbers;
-
-
-/*
-import React, { useEffect, useState } from 'react';
-import { db } from '../../firebase';
-import './PhoneNumbers.css';
-
-const predefinedPhoneNumbers = {
-  'Organization A': ['123-456-7890', '987-654-3210'],
-  'Organization B': ['555-555-5555'],
-  'Organization C': ['999-999-9999'],
-};
-
-const PhoneNumbers = ({ organization }) => {
-  // const [phoneNumbers, setPhoneNumbers] = useState([]);
-  const phoneNumbers = predefinedPhoneNumbers[organization] || [];
-
-  // useEffect(() => {
-  //   const fetchPhoneNumbers = async () => {
-  //     try {
-  //       const phoneNumbersCollection = db.collection('input-group');
-  //       const query = phoneNumbersCollection.where('organization', '==', organization); // Filter by organization
-  //       const snapshot = await query.get();
-  //       const phoneNumbersData = snapshot.docs.map((doc) => doc.data().phone_number);
-  //       setPhoneNumbers(phoneNumbersData);
-  //     } catch (error) {
-  //       console.error('Error fetching phone numbers:', error);
-  //     }
-  //   };
-
-  //   if (organization) {
-  //     fetchPhoneNumbers();
-  //   }
-  // }, [organization]);
-
-  return (
-    <div className="phone-numbers-container">
-      {organization && <h2>Phone Numbers for {organization}</h2>}
-      <ul className="phone-numbers-list">
-        {phoneNumbers.map((phoneNumber, index) => (
-          <li 
-            key={index}
-            className="app__organization-item"
-            // onClick={() => onPhoneNumberClick(phoneNumber)}
-          >
-            {phoneNumber}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default PhoneNumbers;
-*/
