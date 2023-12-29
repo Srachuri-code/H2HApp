b571ee174f7ab601a1c83ef2f94d8816e12bfd89
srachuri123 <sujan.k.rachuri@Vanderbilt.edu>
2023-12-28 19:51:23 -0500

    displaying organizations in the dashboard

--- src/container/Organizations/Organizations.jsx
+++ src/container/Organizations/Organizations.jsx
@@ -1,21 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Organizations.css';
+import { db } from "../../firebase";
+import { addDoc, collection, serverTimestamp, getDocs, query, where } from 'firebase/firestore';
+
 
 const predefinedOrganizations = ['Organization A', 'Organization B', 'Organization C'];
 
 const Organizations = ({ onSelectOrganization }) => {
-  const [selectedOrganization, setSelectedOrganization] = useState('');
+  const [organizations, setOrganizations] = useState([]);
+  const [selectedOrganization, setSelectedOrganization] = useState(null);
 
   const handleClick = (organization) => {
     setSelectedOrganization(organization);
     onSelectOrganization(organization);
   };
 
+  useEffect(() => {
+    const fetchOrganizations = async () => {
+      try {
+        const organizationsCollection = collection(db, 'input-group');
+        const snapshot = await getDocs(organizationsCollection);
+        const organizationsList = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          const organizationsData = data.organization;
+          organizationsList.push(organizationsData);
+        });
+        setOrganizations(organizationsList);
+      } catch (error) {
+        console.error('error fetching organizations:', error);
+      }
+    };
+    fetchOrganizations(); 
+  }, []); 
+
   return (
     <div className="app__organizations-container">
       <h2>Choose an organization:</h2>
       <ul className="app__organizations">
-        {predefinedOrganizations.map((organization) => (
+        {organizations.map((organization) => (
           <li
             className={`app__organization-item ${selectedOrganization === organization ? 'active' : ''}`}
             key={organization}
