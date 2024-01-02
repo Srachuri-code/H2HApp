7382f39405cdbdf9e10bdb254ebd9905f3e332c9
eliasxlevy <elias.levy@vanderbilt.edu>
2024-01-02 14:18:37 -0500

    Tab Logo

--- server/index.js
+++ server/index.js
@@ -36,10 +36,6 @@ app.post('/api/messages', (req, res) => {
     });
 });
 
-// app.listen(3001, () =>
-//   console.log('Express server is running on localhost:3001')
-// );
-
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
