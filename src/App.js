a6241d21e104ca4a8f37766a2d78a5ad7bf2fe27
srachuri123 <sujan.k.rachuri@Vanderbilt.edu>
2024-02-05 23:24:47 -0600

    styled sign in

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Organizations, PhoneNumbers, Metrics, SMSForm } from './container';
 import { images } from './components';
 import { auth, signInWithGoogle, signOut } from './firebase';
 
-const allowedUsers = ["sujan.rachuri@gmail.com", "sujan.k.rachuri@vanderbilt.edu"]; // Add your allowed user's email
+const allowedUsers = ["sujan.rachuri@gmail.com", "sujan.k.rachuri@vanderbilt.edu", "elias.levy@vanderbilt.edu", "general@thechec.org"]; // Add your allowed user's email
 
 const App = () => {
   const [selectedOrganization, setSelectedOrganization] = useState('');
@@ -53,15 +53,15 @@ const App = () => {
 
   return (
     <div className='app'>
-      <div className='header'>
+      <div className='welcome-header'>
         {user && allowedUsers.includes(user.email) && (
           <div className='user-welcome'>
-            <p>Welcome, {user.displayName || user.email}!</p>
+            <p>Welcome, {user.displayName || user.email}</p>
           </div>
         )}
 
         {user && allowedUsers.includes(user.email) && (
-          <div className='sign-out-button'>
+          <div className='sign-out-button' class="button">
             <button onClick={handleSignOutClick}>Sign Out</button>
           </div>
         )}
@@ -91,7 +91,7 @@ const App = () => {
           <div className='sign-in-container'>
             <h2>Heart2Heart Admin Dashboard</h2>
             {!user || (user && allowedUsers.includes(user.email)) ? (
-              <button onClick={handleSignInClick}>Sign in With Google</button>
+              <button class="button" onClick={handleSignInClick}>Sign in With Google</button>
             ) : (
               <p>This email is not approved to view the dashboard.</p>
             )}
